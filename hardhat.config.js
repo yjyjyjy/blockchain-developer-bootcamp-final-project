429adaa8a2c241f5d4f5bea24d865bcc9e708874
Junyu Yang <junyuyang@Junyus-MacBook-Pro.local>
2021-11-05 22:20:10 -0700

    change

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,6 +18,7 @@ module.exports = {
       url: 'https://eth-kovan.alchemyapi.io/v2/9uMRBt5ifCM5w3YExYefwftWntny7H9Q',
       // accounts: [`0x${process.env.KOVAN_PRIVATE_KEY}`]
       accounts: ['0x61c3cddbca209b7197187cbaf98accf5e7dd4fe3276f7d3444ab955dc035b4ef']
+
     }
   }
 };

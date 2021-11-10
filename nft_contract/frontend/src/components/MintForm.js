65a6fc978329156920f2616e6469c876dbff029d
Junyu Yang <prowessyang@gmail.com>
2021-11-10 09:34:56 -0800

    new file structure

--- nft_contract/frontend/src/components/MintForm.js
+++ nft_contract/frontend/src/components/MintForm.js
@@ -2,7 +2,38 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-input-slider';
+import { ethers } from "ethers";
 
+const RINKEBY_CONTRACT_ADDRESS = '0x0476eC77191623e22D6B73dC43998a27997d435d';
+
+const mint = async num => {
+  console.log(`minting ${num}`);
+
+  // try {
+  //   if (!window.ethereum) {
+  //     console.log("No crypto wallet found. Please install it.");
+  //     throw new Error("No crypto wallet found. Please install it.")
+  //   }
+
+  //   // await window.ethereum.send("eth_requestAccounts");
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //   const signer = provider.getSigner();
+  //   ethers.utils.getAddress(addr);
+  //   const tx = await signer.sendTransaction({
+  //     to: addr,
+  //     value: ethers.utils.parseEther(ether)
+  //   });
+  //   console.log({ ether, addr });
+  //   console.log("tx", tx);
+  //   setTxs([tx]);
+  // } catch (err) {
+  //   setError(err.message);
+  // }
+}
+
+const connectWallet = async () => {
+  console.log('connecting');
+}
 
 // Display: address, Amount of ether I have, which network
 // Form: need # of nfts to mint, > mint
@@ -10,7 +41,11 @@ import Slider from 'react-input-slider';
 // need error handling
 // (nice to have) need state display for the transaction
 const MintForm = props => {
+
   const [formData, setFormData] = useState({ numberToMint: 3 });
+
+
+
   const onSubmit = e => {
     e.preventDefault();
     console.log(formData)

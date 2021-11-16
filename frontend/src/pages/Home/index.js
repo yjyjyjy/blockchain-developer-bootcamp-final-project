8fc8a73e4c7e200c9f83ab9dfa6057056ce20230
Junyu Yang <prowessyang@gmail.com>
2021-11-15 22:44:32 -0800

    cleaned packages

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,17 +3,14 @@ import { Container } from 'react-bootstrap';
 import NftInteractionCard from './NftInteractionCard';
 import ConnectWalletModal from '../../components/ConnectWalletModal';
 import useWalletConnectionModal from '../../hooks/useWalletConnectionModal';
-import { StateInspector } from "reinspect"
 
 const Home = () => {
   const { isWalletConnectModalOpen } = useWalletConnectionModal();
   return (
-    <StateInspector name="App">
-      <Container className="mt-5">
-        {isWalletConnectModalOpen && <ConnectWalletModal />}
-        <NftInteractionCard />
-      </Container>
-    </StateInspector>
+    <Container className="mt-5">
+      {isWalletConnectModalOpen && <ConnectWalletModal />}
+      <NftInteractionCard />
+    </Container>
   );
 };
 
